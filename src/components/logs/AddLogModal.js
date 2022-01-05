9e433d1d4b48fb095e94be0f989021d682e2a048
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-01-06 01:37:28 +0300

    initial commit

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,111 +1,89 @@
 import React, { useState } from 'react';
-import TechSelectOptions from '../techs/TechSelectOptions';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { addLog } from '../../actions/logActions';
-import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = ({ addLog }) => {
-  const [message, setMessage] = useState('');
-  const [attention, setAttention] = useState(false);
-  const [tech, setTech] = useState('');
+const AddLogModal = () => {
+  const [logMessage, setLogMessage] = useState("")
+  const [attention, setAttention] = useState(false)
+  const [technician, setTechnician] = useState("")
 
   const onSubmit = () => {
-    if (message === '' || tech === '') {
-      M.toast({ html: 'Please enter a message and tech' });
+    if (logMessage === "" || technician === "") {
+      M.toast({ html: "Please include a log message and a technician" })
     } else {
-      const newLog = {
-        message,
-        attention,
-        tech,
-        date: new Date()
-      };
-
-      addLog(newLog);
-
-      M.toast({ html: `Log added by ${tech}` });
-
-      // Clear Fields
-      setMessage('');
-      setTech('');
-      setAttention(false);
+      console.log(logMessage, attention, technician)
+      setLogMessage("")
+      setAttention(false)
+      setTechnician("")
     }
-  };
-
+  }
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
-      <div className='modal-content'>
+    <div id="add-log-modal" className="modal" style={modalStyle}>
+      <div className="modal-content">
         <h4>Enter System Log</h4>
-        <div className='row'>
-          <div className='input-field'>
+        <div className="row">
+          <div className="input-field">
             <input
-              type='text'
-              name='message'
-              value={message}
-              onChange={e => setMessage(e.target.value)}
+              type="text"
+              name="message"
+              value={logMessage}
+              onChange={(e) => setLogMessage(e.target.value)}
             />
-            <label htmlFor='message' className='active'>
+            <label htmlFor="message" className="active">
               Log Message
             </label>
           </div>
         </div>
 
-        <div className='row'>
-          <div className='input-field'>
+        <div className="row">
+          <div className="input-field">
             <select
-              name='tech'
-              value={tech}
-              className='browser-default'
-              onChange={e => setTech(e.target.value)}
+              name="technician"
+              value={technician}
+              className="browser-default"
+              onChange={(e) => setTechnician(e.target.value)}
             >
-              <option value='' disabled>
+              <option value="" disabled>
                 Select Technician
               </option>
-              <TechSelectOptions />
+              <option value="John Mcafee">John McaFee</option>
+              <option value="John Maurice">John Maurice</option>
             </select>
           </div>
         </div>
 
-        <div className='row'>
-          <div className='input-field'>
+        <div className="row">
+          <div className="input-field">
             <p>
               <label>
                 <input
-                  type='checkbox'
-                  className='filled-in'
+                  type="checkbox"
+                  className="filled-in"
                   checked={attention}
                   value={attention}
-                  onChange={e => setAttention(!attention)}
+                  onChange={(e) => setAttention(!attention)}
                 />
-                <span>Needs Attention</span>
+                <span>Needs attention</span>
               </label>
             </p>
           </div>
         </div>
       </div>
-      <div className='modal-footer'>
+
+      <div className="modal-footer">
         <a
-          href='#!'
+          href="#!"
           onClick={onSubmit}
-          className='modal-close waves-effect blue waves-light btn'
+          className="modal-close waves-effect waves-light btn"
         >
           Enter
         </a>
       </div>
     </div>
-  );
-};
-
-AddLogModal.propTypes = {
-  addLog: PropTypes.func.isRequired
-};
+  )
+}
 
 const modalStyle = {
-  width: '75%',
-  height: '75%'
-};
+  width: "75%",
+  height: "75%",
+}
 
-export default connect(
-  null,
-  { addLog }
-)(AddLogModal);
+export default AddLogModal
